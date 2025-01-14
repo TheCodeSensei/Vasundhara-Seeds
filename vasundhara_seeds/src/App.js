dbd9ace2970dc52c61b55af9deea4278b86fc27a
TheCodeSensei <ninad.abhyankar88@gmail.com>
2025-01-14 21:53:20 +0530

    adding react files. added a logo and a Navbar

---
+++ vasundhara_seeds/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './components/Navbar'
+
+
+function App() {
+ return(
+  <div>
+    <img src='/logo.jpg' alt = 'Logo' width='150' />
+    <Navbar />
+  </div>
+
+ );
+}
+
+export default App;
